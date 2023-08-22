f763ed0fb2fe621991627194aaf4350249825832
HoAnhKhoa <traincolab2131999v1@gmail.com>
2023-08-22 14:00:46 +0700

    Add code to postprocess tranlasted text

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import argparse
 import os
 # from my_ocr.easyocr import ocr
 from my_ocr.pp_ocr.test import ocr
-from translate.hvdict import hvdic_translate
-# from translate.hcmus_api import hcmus_translate
+# from translate.hvdict import hvdic_translate
+from translate.hcmus_api import hcmus_translate
 from my_postprocess.postprocess import _postprocess
 import logging
 from typing import Text
@@ -36,8 +36,8 @@ def process(
         print(f'bbox: {bbox}')
         print(f'text: {han_nom_script}')
         logging.debug(f"han_nom_script : {han_nom_script}")
-        translation_script = hvdic_translate(text = han_nom_script)
-        # translation_script = hcmus_translate(text = han_nom_script)
+        # translation_script = hvdic_translate(text = han_nom_script)
+        translation_script = hcmus_translate(text = han_nom_script)
         list_dict_result.append({
             'bbox': bbox,
             'text': translation_script
@@ -86,14 +86,14 @@ def main():
 if __name__ == "__main__":
     # main()
 
-    img: Image = process(
-        image_input_file="D:/Master/OCR_Nom/fulllow_ocr_temple/input/365277540_2640178542812959_3109842896588336028_n.jpg"
-    )
-    img.save('output/365277540_2640178542812959_3109842896588336028_n_pdocr.jpg')
-
-
-
     # img: Image = process(
-    #     image_input_file="D:/Master/OCR_Nom/fulllow_ocr_temple/input/vertical_demo.png"
+    #     image_input_file="D:/Master/OCR_Nom/fulllow_ocr_temple/input/365277540_2640178542812959_3109842896588336028_n.jpg"
     # )
-    # img.save('output/vertical_demo_pdocr.png')
+    # img.save('output/365277540_2640178542812959_3109842896588336028_n_pdocr.jpg')
+
+
+
+    img: Image = process(
+        image_input_file="D:/Master/OCR_Nom/fulllow_ocr_temple/input/vertical_demo.png"
+    )
+    img.save('output/vertical_demo_pdocr.png')
--- translate/hcmus_api.py
+++ translate/hcmus_api.py
@@ -1,11 +1,21 @@
 import json
 import time
 import requests
+from typing import Text
 
 ERR_TRANSLATE_STR = 'Cannot translate this text.'
 
+def postproces_hcmus_translation(
+    text: Text
+)-> Text:
+    lst_text = text.split()
+    lst_capitalize_text = list(map(lambda x: x.capitalize(), lst_text))
+    res = ' '.join(lst_capitalize_text) 
+    return res
 
-def hcmus_translate(text):
+def hcmus_translate(
+    text: Text
+    ):
     url = ' https://api.clc.hcmus.edu.vn/nom_translation/90/1'
     response = requests.request('POST', url, data={'nom_text': text})
     time.sleep(0.3)
@@ -13,6 +23,10 @@ def hcmus_translate(text):
     try:
         result = json.loads(response.text)['sentences']
         result = result[0][0]['pair']['modern_text']
+
+        # region post processing
+        result = postproces_hcmus_translation(text = result)
+        # endregion 
         return result
     except:
         print(f'[ERR] "{text}": {response.text}')
