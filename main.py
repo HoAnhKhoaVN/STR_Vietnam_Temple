48a28420fd1cdc251ccc9213030fc664a93ea1f8
HoAnhKhoa <traincolab2131999v1@gmail.com>
2023-09-11 15:27:21 +0700

    Add code analysis time

--- main.py
+++ main.py
@@ -14,14 +14,19 @@ setup_logger()
 
 def process(
     image_input_file: Text,
+    silent: bool = False
 )-> Image:
-    start_time = time()
+    time_analysis = dict()
     # region Preprocessing
     # endregion Preprocessing
 
-    # region OCR    
-    result = ocr(image_input_file)
-    print(f'Result: {result}')
+    # region OCR
+    start_time = time()
+    result, time_analysis = ocr(image_input_file, time_analysis)
+    end_time = time()
+    time_analysis['full_ocr_time'] = end_time - start_time
+    if not silent:
+        print(f'Result: {result}')
 
     # endregion OCR
     logging.debug(f"Before: {result}")
@@ -31,6 +36,8 @@ def process(
     # endregion
 
     # region translate to Vietnamese
+    start_time = time()
+
     list_dict_result = []
     for bbox, han_nom_script, _ in result:
         print(f'bbox: {bbox}')
@@ -43,18 +50,24 @@ def process(
             'text': translation_script
             }
         )
+    end_time = time()
+    time_analysis['translate_time'] = end_time - start_time
     # endregion
-    print(f'list_dict_result: {list_dict_result}')
+    if not silent:
+        print(f'list_dict_result: {list_dict_result}')
     logging.debug(f"After: {list_dict_result}")
     # region postprocessing
+    start_time = time()
     pil_img_output = _postprocess(
         image_fn= image_input_file,
         list_dict_result = list_dict_result,
     )
+    end_time = time()
+    time_analysis['postprocess_time'] = end_time - start_time
     # endregion
     end_time = time()
     logging.info(f"Time inference: {(end_time-start_time)}s")
-    return pil_img_output
+    return pil_img_output, time_analysis
 
 
 def main():
@@ -75,11 +88,16 @@ def main():
     logging.info(f"Output: {_OUTPUT}")
     # endregion Input
 
-    pil_img_output = process(image_input_file=_INPUT)
+    pil_img_output, time_analysis = process(image_input_file=_INPUT)
 
     # region output
     pil_img_output.save(_OUTPUT)
     pil_img_output.show()
+    # endregion
+
+    # region Analysis time
+    print(time_analysis)
+
     # endregion
     logging.info("==============END TASK==================")
 
@@ -93,7 +111,10 @@ if __name__ == "__main__":
 
 
 
-    img: Image = process(
+    img, time_analysis = process(
         image_input_file="D:/Master/OCR_Nom/fulllow_ocr_temple/input/13925854_308100976209095_8956468595154727390_o.jpg",
     )
+
     img.save('output/13925854_308100976209095_8956468595154727390_o.png')
+
+    print(f'Time analysis: {time_analysis}')
--- my_ocr/pp_ocr/test.py
+++ my_ocr/pp_ocr/test.py
@@ -1,15 +1,21 @@
-from log.logger import setup_logger
-setup_logger()
+# from log.logger import setup_logger
+# setup_logger()
 from paddleocr import PaddleOCR
 from typing import List
 pdocr = PaddleOCR(use_angle_cls=True, lang='ch')
+from time import time
 
 
 def ocr(
-    img_path : str
+    img_path : str,
+    time_analysis: dict = {}
 )-> List:
+    s = time()
     result = pdocr.ocr(img_path, cls=True)[0]
+    e = time()
+    time_analysis['ppocr_time'] = e - s
 
+    s = time()
     res= []
     # region Postprocessing
     for line in result:
@@ -17,15 +23,18 @@ def ocr(
         bbox = [[int(bb[0]), int(bb[1])] for bb in _bbox]
         res.append([bbox, text, prop])
     # endregion
-    return res
+    e = time()
+    time_analysis['ppocr_postprocess'] = e - s
+    return res, time_analysis
     
 
 
 if __name__ == "__main__":
-    img_path = "D:/Master/OCR_Nom/fulllow_ocr_temple/input/demo_img.png"
-    result = ocr(img_path)
+    img_path = "D:/Master/OCR_Nom/fulllow_ocr_temple/input/13925854_308100976209095_8956468595154727390_o.jpg"
+    result, time_analysis = ocr(img_path)
     bbox, text, prop = result[0]
     print(f'bbox: {bbox}')
     print(f'text: {text} - prop: {prop}')
+    print(f'Time analysis: {time_analysis}')
         
 
