bcdcfa3be8e98fd53c0d2696f182a0174b336291
HoAnhKhoa <traincolab2131999v1@gmail.com>
2023-09-27 14:11:51 +0700

    Add code to test postprocess of PP-OCR

---
+++ my_ocr/pp_ocr/tool/infer/infer.sh
@@ -0,0 +1,5 @@
+python predict_system.py \
+        --image_dir="D:/Master/OCR_Nom/fulllow_ocr_temple/input/13718780_847719332039821_170320190789157617_n.jpg" \
+        --det_model_dir="C:/Users/AnhKhoa/.paddleocr/whl/det/ch/ch_PP-OCRv4_det_infer" \
+        --cls_model_dir="C:/Users/AnhKhoa/.paddleocr/whl/cls/ch_ppocr_mobile_v2.0_cls_infer"  \
+        --rec_char_dict_path="C:/Users/AnhKhoa/.paddleocr/whl/rec/ch/ch_PP-OCRv4_rec_infer"
