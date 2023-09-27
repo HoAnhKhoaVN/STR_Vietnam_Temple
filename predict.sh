a2d4be35e0135c5f857c9bed1523044b5a81d600
HoAnhKhoa <traincolab2131999v1@gmail.com>
2023-09-27 14:28:21 +0700

    Change path virtual env

--- predict.sh
+++ predict.sh
@@ -1,4 +1,5 @@
-source D:/Master/OCR_Nom/deploy/azure/str_vietnam_temple/.venv/Scripts/activate
+# source D:/Master/OCR_Nom/deploy/azure/str_vietnam_temple/.venv/Scripts/activate
+source D:/Master/OCR_Nom/fulllow_ocr_temple/.venv/Scripts/activate
 
 INPUT='input_2'
 OUTPUT='output_2'
